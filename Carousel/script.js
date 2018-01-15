51bb4867dc895aee86fd2ec7dc8b7182e96e3ad5
BeomyeonAndrewKim <beom911@gmail.com>
2018-01-15 23:49:04 +0900

    height값 버그 수정

--- Carousel/script.js
+++ Carousel/script.js
@@ -1,24 +1,42 @@
 let prev = document.querySelector('.previous');
 let next = document.querySelector('.next');
-let page = 1;
-prev.addEventListener('click', e => {
+let page = 0;
+let slide1 = document.querySelector('.slide-1');
+let slide2 = document.querySelector('.slide-2');
+let slide3 = document.querySelector('.slide-3');
+// prev.addEventListener('click', e => {
 
-        if (page === 1) {
-            document.querySelector('.slide-2').classList.add('slidein', 'show');
-            page += 1;
-            console.log(page);
-        } else if (page === 2) {
-            document.querySelector('.slide-3').classList.add('slidein', 'show');
-            document.querySelector('.slide-1').classList.remove('show');
-            page += 1;
-            console.log(page);
-        }
-        if (page === 3) {
-            document.querySelector('.slide-1').classList.add('slidein');
-            document.querySelector('.slide-2').classList.remove('show');
-            page = 1;
-            console.log(page);
-        }
-    })
-    // next.addEventListener('click', e => {
-    // })
+//     if (page === 0) {
+//         slide2.classList.replace('slideoutL', 'slideinL');
+//         slide1.classList.add('slideoutL');
+//         page += 1;
+//         console.log(page);
+//     } else if (page === 1) {
+//         slide3.classList.replace('slideoutL', 'slideinL');
+//         slide2.classList.replace('slideinL', 'slideoutL');
+//         page += 1;
+//         console.log(page);
+//     } else if (page === 2) {
+//         slide1.classList.replace('slideoutL', 'slideinL');
+//         slide3.classList.replace('slideinL', 'slideoutL');
+//         page += 1;
+//         console.log(page);
+//     } else if (page === 3) {
+//         slide2.classList.replace('slideoutL', 'slideinL');
+//         slide1.classList.replace('slideinL', 'slideoutL')
+//         page = 1;
+//         console.log(page);
+//     }
+// })
+// next.addEventListener('click', e => {
+//     if (page === 0) {
+//         slide3.classList.replace('slideoutL', 'slideinR');
+//         slide1.classList.add('slideoutR');
+//         page += 1;
+//         console.log(page);
+//     }
+//     if (page === 1) {
+//         slide3.classList.replace('slideinR', 'slideoutR');
+//         slide2.classList.replace('slideoutL', 'slideinR');
+//     }
+// })
